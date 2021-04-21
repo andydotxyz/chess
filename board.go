ba2bece93a1bec2da1ece46dabbf33a94c72f4db
Andy Williams <andy@andy.xyz>
2021-04-21 19:31:53 +0100

    Update bundling and gather resource code to board

--- board.go
+++ board.go
@@ -1,10 +1,13 @@
 //go:generate fyne bundle -o bundled-board.go board
+//go:generate fyne bundle -o bundled-pices.go pieces
 
 package main
 
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
+
+	"github.com/notnil/chess"
 )
 
 type boardLayout struct{}
@@ -33,4 +36,40 @@ func (b *boardLayout) Layout(cells []fyne.CanvasObject, s fyne.Size) {
 func (b *boardLayout) MinSize(_ []fyne.CanvasObject) fyne.Size {
 	edge := theme.IconInlineSize() * 8
 	return fyne.NewSize(edge, edge)
+}
+
+func resourceForPiece(p chess.Piece) fyne.Resource {
+	switch p.Color() {
+	case chess.Black:
+		switch p.Type() {
+		case chess.Pawn:
+			return resourceBlackPawnSvg
+		case chess.Rook:
+			return resourceBlackRookSvg
+		case chess.Knight:
+			return resourceBlackKnightSvg
+		case chess.Bishop:
+			return resourceBlackBishopSvg
+		case chess.Queen:
+			return resourceBlackQueenSvg
+		case chess.King:
+			return resourceBlackKingSvg
+		}
+	case chess.White:
+		switch p.Type() {
+		case chess.Pawn:
+			return resourceWhitePawnSvg
+		case chess.Rook:
+			return resourceWhiteRookSvg
+		case chess.Knight:
+			return resourceWhiteKnightSvg
+		case chess.Bishop:
+			return resourceWhiteBishopSvg
+		case chess.Queen:
+			return resourceWhiteQueenSvg
+		case chess.King:
+			return resourceWhiteKingSvg
+		}
+	}
+	return nil
 }
